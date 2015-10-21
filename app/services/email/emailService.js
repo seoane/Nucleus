ac56e4ea75a2b1d469fc35686466d52c8fe6ab1f
seoane <suso@outlook.com>
2015-10-21 19:33:22 +0200

    Changing sent Json

--- app/services/email/emailService.js
+++ app/services/email/emailService.js
@@ -8,13 +8,13 @@ angular.module('NucleusWebApp.EmailService', []).
             sendEmail: function (startingDate, endingDate, message, hyperlink) {
                 console.log(startingDate, endingDate, message, hyperlink);
                 url = ApiEndpoint + "/user/sendweeklyreport/";
-                $http.post(url, {startingDate:startingDate,endingDate:endingDate,message:message,hyperlink:hyperlink,testEmailAddress:null});
+                $http.post(url, {from_date:startingDate,to_date:to_date,message:message,hyperlink:hyperlink,test_email:null});
             },
             sendTestEmail: function (startingDate, endingDate, message, hyperlink, testEmailAddress) {
                 console.log(startingDate, endingDate, message, hyperlink)
                 url = ApiEndpoint + "/user/sendweeklyreport/";
-                $http.post(url, {startingDate:startingDate,endingDate:endingDate,message:message,hyperlink:hyperlink,
-                    testEmailAddress:testEmailAddress});
+                $http.post(url, {from_date:startingDate,to_date:to_date,message:message,hyperlink:hyperlink,test_email:testEmailAddress});
+
             }
 
         }
